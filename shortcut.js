761df94dbee3b56da27375d1550c917ade7b1856
Md Asaduzaman <asadasad9282@gmail.com>
2022-03-08 12:22:08 +0600

    toggle

--- shortcut.js
+++ shortcut.js
@@ -36,3 +36,41 @@ if (money > 100) {
 let food1 = money > 100 ? 'fast food' : 'cheap food'
 
 console.log(food1);
+
+let drink = (money > 100 && myMoney > 100) ? 'coke' : 'filter water'
+
+// console.log(drink);
+
+
+//  convart number to string
+const num1 = 44;
+
+const numStr = num1 + ''
+// console.log(numStr);
+
+
+
+//  convart string to number
+let strNum = '4445'
+let num = + strNum
+
+
+
+// 
+let isActive = true;
+const showUser = () => console.log('display user');
+const hideUser = () => console.log('hide user');
+
+isActive ? showUser() : hideUser()
+
+
+// use && if the left side is true then right side will be executed
+isActive && showUser()
+
+
+// use || if the left side is false then right side will be executed
+isActive || hideUserUser()
+
+
+//  toggae boolean
+isActive = !isActive
